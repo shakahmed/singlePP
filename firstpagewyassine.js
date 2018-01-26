1aacd1001d570f0da7e3a8b069b0987391473650
shakahmed <ashakur1999@gmail.com>
2018-01-26 12:27:06 -0800

    first firstpage upload

---
+++ firstpagewyassine.js
@@ -0,0 +1,20 @@
+let h3 = document.querySelectorAll("h3");
+const form = document.querySelector("firstform");
+const button = document.getElementById("firstbutton");
+
+
+h3.onmouseover."style.color = 'green' ";
+
+
+
+
+// <h1 onclick="changeText(this)">Click on this text!</h1>
+
+// <script>
+// function changeText(id) {
+//     id.innerHTML = "Ooops!";
+// }
+// </script>
+
+// </body>
+// </html>
